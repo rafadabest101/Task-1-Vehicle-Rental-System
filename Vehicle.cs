f87386aa1b65b57711bf6f4e08dc8221e2e1da72
Yavor Chervenkov <72622812+rafadabest101@users.noreply.github.com>
2024-06-16 19:21:55 +0300

    Update Vehicle.cs

--- Vehicle.cs
+++ Vehicle.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,11 +8,13 @@ namespace vehicleRentalSystem
 {
     public abstract class Vehicle
     {
+        // Properties used by all child classes
         public string VehicleBrand { get; set; }
         public string VehicleModel { get; set; }
         public int VehicleValue { get; set; }
         public int RentalPeriod { get; set; }
 
+        // Constructor that sets values to the properties
         public Vehicle(string brand, string model, int value, int period)
         {
             VehicleBrand = brand;
@@ -21,6 +23,7 @@ namespace vehicleRentalSystem
             RentalPeriod = period;
         }
 
+        // Abstract methods implemented by all child classes
         public abstract int CalculateRentalCost();
 
         public abstract double CalculateInsuranceCost();
