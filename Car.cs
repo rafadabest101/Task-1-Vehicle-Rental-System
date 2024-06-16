94f88c6cdfd86cf63ebe5e9337202a6003fbcf78
Yavor Chervenkov <72622812+rafadabest101@users.noreply.github.com>
2024-06-16 19:20:23 +0300

    Update Car.cs

--- Car.cs
+++ Car.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,26 +8,33 @@ namespace vehicleRentalSystem
 {
     public class Car : Vehicle
     {
+        // Unique property for this class
         public int SafetyRating { get; set; }
 
+
+        // Constructor that sets values to the properties
         public Car(string brand, string model, int value, int period, int rating)
             : base(brand, model, value, period)
         {
+            // This is used to validate the rating so that it is from 1 to 5
             if(rating > 1 && rating < 5) SafetyRating = rating;
             else throw new Exception("Invalid safety rating!");
         }
 
+        // Calculates total renting cost for the whole rental period
         public override int CalculateRentalCost()
         {
             if (RentalPeriod <= 7) return RentalPeriod * 20;
             else return RentalPeriod * 15;
         }
 
+        // Calculates total insurance cost for the whole rental period
         public override double CalculateInsuranceCost()
         {
             return VehicleValue * 0.01 / 100 * RentalPeriod;
         }
 
+        // Calculates whether the customer will receive an insurance discount based on the safety rating of the car
         public override double CalculateInsuranceChange()
         {
             return (SafetyRating >= 4) ? CalculateInsuranceCost() * 0.1 : 0;
